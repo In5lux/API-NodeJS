ee9605a5ec0bd1a2a4f81d773fe48a18e4caed65
In5lux <nikos_almeryda@mail.ru>
2022-01-31 22:27:41 +0300

    Router Express

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
+
 const port = 8000;
 const app = express();
 
-app.all('/hello', (req, res, next) => {
-	console.log('All');
-	next();
+app.get('/hello', (req, res) => {
+	//res.send('Привет!');
+	res.end();
 });
 
-const cb = (req, res, next) => {
-	console.log('CB');
-	next();
-}
-
-app.get('/hello', cb, (req, res) => {
-	res.send('Привет!');
-});
-
-app.route('/user')
-	.get((req, res) => {
-		res.send('Привет! GET');
-	})
-	.post((req, res) => {
-		res.send('Привет! POST');
-	});
-
-
+app.use('/users', userRouter);
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на http://localhost:${port}`);
