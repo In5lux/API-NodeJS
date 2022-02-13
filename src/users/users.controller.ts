eeec8ec536c6863f65d20e17a4ccd6dd622eb577
In5lux <nikos_almeryda@mail.ru>
2022-02-13 10:04:14 +0300

    Eslint, prettier

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -13,14 +13,14 @@ export class UserController extends BaseController implements IUserController {
 		super(loggerService);
 		this.bindRoutes([
 			{ path: '/register', method: 'post', func: this.register },
-			{ path: '/login', method: 'post', func: this.login }
+			{ path: '/login', method: 'post', func: this.login },
 		]);
 	}
 
-	login(req: Request, res: Response, next: NextFunction) {
+	login(req: Request, res: Response, next: NextFunction): void {
 		next(new HTTPError(401, 'ошибка авторизации', 'login'));
 	}
-	register(req: Request, res: Response, next: NextFunction) {
+	register(req: Request, res: Response, next: NextFunction): void {
 		this.ok(res, 'register');
 	}
 }
