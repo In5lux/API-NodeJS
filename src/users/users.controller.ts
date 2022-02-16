e14da0b875a48c5a0f64595a5b6dc4d1edaa707b
In5lux <nikos_almeryda@mail.ru>
2022-02-16 07:59:58 +0300

    Data transfer object

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,6 +6,8 @@ import { ILogger } from '../logger/logger.interface';
 import { TYPES } from '../types';
 import 'reflect-metadata';
 import { IUserController } from './users.controller.interface';
+import { UserRegisterDto } from './dto/user-register.dto';
+import { UserLoginDto } from './dto/user-login.dto';
 
 @injectable()
 export class UserController extends BaseController implements IUserController {
@@ -17,10 +19,12 @@ export class UserController extends BaseController implements IUserController {
 		]);
 	}
 
-	login(req: Request, res: Response, next: NextFunction): void {
+	login(req: Request<{}, {}, UserLoginDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
 		next(new HTTPError(401, 'ошибка авторизации', 'login'));
 	}
-	register(req: Request, res: Response, next: NextFunction): void {
+	register(req: Request<{}, {}, UserRegisterDto>, res: Response, next: NextFunction): void {
+		console.log(req.body);
 		this.ok(res, 'register');
 	}
 }
