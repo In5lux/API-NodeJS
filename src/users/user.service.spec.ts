4f573fbda676caae48f7027896ec6db4070efe6e
In5lux <nikos_almeryda@mail.ru>
2022-03-16 08:01:02 +0300

    Unit test createUser

---
+++ src/users/user.service.spec.ts
@@ -0,0 +1,54 @@
+import 'reflect-metadata';
+import { UserModel } from '@prisma/client';
+import { Container } from 'inversify';
+import { IConfigService } from '../config/config.service.interface';
+import { TYPES } from '../types';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+import { IUsersRepository } from './users.repository.interface';
+import { IUserService } from './users.service.interface';
+
+const ConfigServiceMock: IConfigService = {
+	get: jest.fn(),
+};
+
+const UsersRepositoryMock: IUsersRepository = {
+	find: jest.fn(),
+	create: jest.fn(),
+};
+
+const container = new Container();
+let configService: IConfigService;
+let usersService: IUserService;
+let usersRepository: IUsersRepository;
+
+beforeAll(() => {
+	container.bind<IUserService>(TYPES.UserService).to(UserService);
+	container.bind<IConfigService>(TYPES.ConfigService).toConstantValue(ConfigServiceMock);
+	container.bind<IUsersRepository>(TYPES.UsersRepository).toConstantValue(UsersRepositoryMock);
+
+	usersService = container.get<IUserService>(TYPES.UserService);
+	configService = container.get<IConfigService>(TYPES.ConfigService);
+	usersRepository = container.get<IUsersRepository>(TYPES.UsersRepository);
+});
+
+let createdUser: UserModel | null;
+
+describe('User Service', () => {
+	it('createUser', async () => {
+		configService.get = jest.fn().mockResolvedValueOnce('1');
+		usersRepository.create = jest.fn().mockImplementationOnce((user: UserModel) => ({
+			name: user.name,
+			email: user.email,
+			password: user.password,
+			id: 1,
+		}));
+		createdUser = await usersService.createUser({
+			email: 'n@n.ru',
+			name: 'Alex',
+			password: '1',
+		});
+		expect(createdUser?.id).toEqual(1);
+		expect(createdUser?.password).not.toEqual('1');
+	});
+});
